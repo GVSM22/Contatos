dd005dffbfed9cbf526c640056f452d84cf48312
unknown <giacomomagri@hotmail.com>
2021-06-22 22:01:57 -0300

    botão improvisado pra abrir o teclado com o número do contato'

--- app/src/main/java/com/example/contatos/ContactActivity.kt
+++ app/src/main/java/com/example/contatos/ContactActivity.kt
@@ -1,7 +1,10 @@
 package com.example.contatos
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.contatos.model.Contact
 
 class ContactActivity : AppCompatActivity() {
@@ -9,5 +12,11 @@ class ContactActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
         val contact: Contact? = intent.getSerializableExtra("contact") as? Contact
+        val callBtn: Button = findViewById(R.id.callButton)
+
+        callBtn.setOnClickListener {
+            val newIntent = Intent(Intent.ACTION_DIAL, Uri.parse(contact?.phoneNumber?.trim()))
+            startActivity(newIntent)
+        }
     }
 }
